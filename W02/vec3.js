f705828654ac4ec1a5af89cf19ea0d8cda20eaed
SALOMON PACO GABIN <195xi01x@istc-a-072.epc.center.kobe-u.ac.jp>
2019-04-16 13:54:59 +0900

    task 1 and 2

--- W02/vec3.js
+++ W02/vec3.js
@@ -22,39 +22,27 @@ Vec3.prototype.sum = function(){
 
 //Search minimum value
 Vec3.prototype.mini = function(){
-	if (this.x<=this.y && this.x <=this.z){
-		mini = this.x;
-	}else if (this.y <= this.x && this.y <= this.z){
-		mini = this.y;
-	}else{
-		mini = this.z;
-	}
-	return mini;
+	var values = [this.x,this.y,this.z];
+	return values.sort()[0];
 }
 
 //Search maxi value
 Vec3.prototype.maxi = function(){
-	if (this.x>=this.y && this.x >=this.z){
-		maxi = this.x;
-	}else if (this.y >= this.x && this.y >= this.z){
-		maxi = this.y;
-	}else{
-		maxi = this.z;
-	}
-	return maxi;
+	var values = [this.x,this.y,this.z];
+	return values.sort()[2];
 }
 
 //Search mid value
 Vec3.prototype.mid = function(){
-	if (this.x != this.maxi() && this.x != this.mini()){
-		mid = this.x;
-	}else if (this.y != this.maxi() && this.y != this.mini()){
-		mid = this.y;
-	}else{
-		mid = this.z;
-	}
+	var values = [this.x,this.y,this.z];
+	return values.sort()[1];
 }
 
 function AreaOfTriangle(v1, v2, v3){
-	
-
+	var s1 = Math.sqrt(Math.pow((v1.x-v2.x),2)+Math.pow((v1.y-v2.y),2)+Math.pow((v1.z-v2.z),2))
+	var s2 = Math.sqrt(Math.pow((v1.x-v3.x),2)+Math.pow((v1.y-v3.y),2)+Math.pow((v1.z-v3.z),2))
+	var s3 = Math.sqrt(Math.pow((v2.x-v3.x),2)+Math.pow((v1.y-v2.y),2)+Math.pow((v2.z-v3.z),2))
+	var s = (s1 + s2 + s3)/2;
+	var area =  Math.sqrt(s*((s-s1)*(s-s2)*(s-s3)));
+	return area;
+}
