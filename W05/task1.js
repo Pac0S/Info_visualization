ccf2750b46b58c3aef5730f4f11035d415e35317
SALOMON PACO GABIN <195xi01x@istc-a-072.epc.center.kobe-u.ac.jp>
2019-05-07 15:30:57 +0900

    Troisieme essai changement nom

--- W05/task1.js
+++ W05/task1.js
@@ -134,6 +134,7 @@ function main()
     
     target = new THREE.Vector3();
 	camera.lookAt(cube.getWorldPosition(target));
+    renderer.render( scene, camera );
 
     loop();
 
@@ -147,4 +148,33 @@ function main()
         cam = camera.lookAt(cube.getWorldPosition(target));
         renderer.render( scene, camera );
     }
+    
+//    document.addEventListener( 'mousedown', mouse_down_event );
+//    function mouse_down_event( event )
+//    {
+//        // Mouse picking
+//        // ...
+//    }
+//    var raycaster = new THREE.Raycaster( origin, direction );
+//    var intersects = raycaster.intersectObject( triangle );
+//    if ( intersects.length > 0 )
+//    {
+//        intersects[0].face.color.setRGB( 1, 0, 0 );
+//        intersects[0].object.geometry.colorsNeedUpdate = true;
+//    }
+//    var x_win = event.clientX;
+//    var y_win = event.clientY;
+//    
+//    var vx = renderer.domElement.offsetLeft;
+//    var vy = renderer.domElement.offsetTop;
+//    var vw = renderer.domElement.width;
+//    var vh = renderer.domElement.height;
+//    var x_NDC = 2 * ( x_win - vx ) / vw - 1;
+//    var y_NDC = -( 2 * ( y_win - vy ) / vh - 1 );
+//    
+//    var p_NDC = new THREE.Vector3( x_NDC, y_NDC, 1 );
+//    var p_wld = p_NDC.unproject( camera );
+//    
+//    var origin = (5,5,5);
+//    var direction = (0,0,0);
 }
