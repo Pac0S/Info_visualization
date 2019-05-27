3dfd3da095dd40eb12b73507def95c241a86fd5c
Paco <paco.salomon@gmail.com>
2019-05-27 20:25:05 +0900

    task1 done

---
+++ W09/w09_main_ex02.js
@@ -0,0 +1,31 @@
+function main()
+{
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+
+    screen.init( volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+
+    var isovalue = 128;
+    var col1 = 1;
+    var col2 = 255;
+    var col3 = 128;
+    var surfaces = Isosurfaces( volume, isovalue, col1, col2, col3 );
+    screen.scene.add( surfaces );
+
+    document.addEventListener( 'mousemove', function() {
+        screen.light.position.copy( screen.camera.position );
+    });
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth, window.innerHeight ] );
+    });
+
+    screen.loop();
+}
