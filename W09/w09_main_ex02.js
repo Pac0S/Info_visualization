d753aa4f297f9f2b9fbb3d0a595c2f23fc116ec7
SALOMON PACO GABIN <195xi01x@istc-c-020.epc.center.kobe-u.ac.jp>
2019-06-14 11:53:26 +0900

    add isovalue slider

--- W09/w09_main_ex02.js
+++ W09/w09_main_ex02.js
@@ -1,20 +1,32 @@
 function main()
 {
     var volume = new KVS.LobsterData();
+    //KVS.CreateTornadoData(100,100,100)
     var screen = new KVS.THREEScreen();
 
     screen.init( volume, {
         width: window.innerWidth,
         height: window.innerHeight,
-        enableAutoResize: false
+        //enableAutoResize: false
+        targetDom: document.getElementById('display'),
+        enableAutoResize: true
     });
 
     var bounds = Bounds( volume );
     screen.scene.add( bounds );
 
-    var isovalue = 230;
+    //var isovalue = 230;
+    var isovalue = document.getElementById("isovalue").value;
     var surfaces = Isosurfaces( volume, isovalue);
     screen.scene.add( surfaces );
+    
+    
+    document.getElementById('isovalue').addEventListener('mousemove', function () {
+        screen.scene.remove(surfaces);
+        isovalue = document.getElementById("isovalue").value;
+        surfaces = Isosurfaces( volume, isovalue);
+        screen.scene.add(surfaces);
+    });
 
     document.addEventListener( 'mousemove', function() {
         screen.light.position.copy( screen.camera.position );
